7b7a99c41b2a24d30e177b76e4ba785d4c1304be
Andrew Owino <andystudy4@gmail.com>
2023-04-13 03:54:46 +0300

    Ignore first forecast record

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   def weather
     @city = params[:city]
     @weather = GetCurrentWeatherData.call(city: @city)
-    @forecast_data = GetForecastWeatherData.call(city: @city)
+    @forecast_data = GetForecastWeatherData.call(city: @city).drop(1) # Ignore first record
   end
 
   def search_cities
