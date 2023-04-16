7724cf3f849f0808d7225a33d2cd3d560bbe9216
Andrew Owino <andystudy4@gmail.com>
2023-04-16 15:14:17 +0300

    Show flash messages

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -3,7 +3,16 @@ class CitiesController < ApplicationController
     city = current_user.cities.build(name: params[:city_name])
     if city.save
       respond_to do |format|
-        format.html { redirect_to favorite_cities_path }
+        format.html {
+          flash[:notice] = "Added to favorites successfully."
+          redirect_to favorite_cities_path
+        }
+      end
+    else
+      respond_to do |format|
+        format.html {
+          redirect_to favorite_cities_path
+        }
       end
     end
   end
