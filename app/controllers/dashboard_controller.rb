2fc54b163bc11c371d66e8e72080eb0b71d8fe47
Andrew Owino <andystudy4@gmail.com>
2023-04-16 23:37:09 +0300

    Add html and json distributions

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,8 +2,13 @@ class DashboardController < ApplicationController
   def weather
     @location = { name: params[:location], country_code: params[:country_code] }
     response = GetWeatherData.call(location: @location)
-    @weather = response.first["weather"]
+    @current_weather = response.first["weather"].first
     @forecast_data = response.drop(1) # Ignore first record which is the current weather
+
+    respond_to do |format|
+      format.html
+      format.json { render json: { location: @location, current_weather: @current_weather, forecast_data: @forecast_data }, status: :ok }
+    end
   end
 
   def search_locations
@@ -12,10 +17,19 @@ class DashboardController < ApplicationController
       @locations = SearchLocations.search_locations(search_query)
       if @locations.count == 0
         flash.now[:warning] = "Location not found"
-        render "search_locations"
+
+        respond_to do |format|
+          format.html { render "search_locations" }
+          format.json { render json: { error: "Location not found" }, status: :not_found }
+        end
       end
     else
       @locations = []
+
+      respond_to do |format|
+        format.html
+        format.json { render json: { locations: @locations } }
+      end
     end
   end
 end
