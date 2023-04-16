4775202c6bc8d503317a6f17fccb25cbf502da01
Andrew Owino <andystudy4@gmail.com>
2023-04-16 13:40:52 +0300

    Add cities

---
+++ db/migrate/20230416103832_create_cities.rb
@@ -0,0 +1,11 @@
+class CreateCities < ActiveRecord::Migration[6.1]
+  def change
+    create_table :cities do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :name
+      t.string :country
+
+      t.timestamps
+    end
+  end
+end
